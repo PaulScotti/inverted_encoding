20346eb8a9352223c58ea53d9a308fcbfcab0119
Paul Scotti <paulpauzle@gmail.com>
2021-01-20 13:20:24 -0500

    move to initpy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name='inverted_encoding',
-    version='0.0.12',
+    version='0.0.13',
     description='Implementation of inverted encoding model as described in Scotti, Chen, & Golomb',
     py_modules=["inverted_encoding"],
     package_dir={'':'src'},
