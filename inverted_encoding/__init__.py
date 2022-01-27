675814cc314f5f5af72e707835858ef4446a277f
Paul Scotti <paulpauzle@gmail.com>
2022-01-27 11:23:23 -0500

    Revert "updates prior to Neuron submission"
    This reverts commit 4aa663adca35d6675e78e9d8f103c29ebcc91f28.

--- inverted_encoding/__init__.py
+++ inverted_encoding/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.9'
+__version__ = '0.0.2'
 
 ## Import packages
 import numpy as np
@@ -7,15 +7,30 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 ## Convenience functions
+k2sd = lambda k : np.sqrt( -2 * np.log( sp.special.iv(1,k) / sp.special.iv(0,k) ) )
+def sd2k(S):
+    try:
+        len(S)
+    except:
+        S = np.array([S])
+    R = np.exp(-S**2/2)
+    K = 1/(R**3 - 4 * R**2 + 3 * R)
+    if np.any(R < 0.85):
+        K[R < 0.85] = -0.4 + 1.39 * R[R < 0.85] + 0.43/(1 - R[R < 0.85])
+    if np.any(R < 0.53):
+        K[R < 0.53] = 2 * R[R < 0.53] + R[R < 0.53]**3 + (5 * R[R < 0.53]**5)/6
+    if len(S) == 1:
+        K = float(K)
+    return K
 def normalize(x):
     return (x-np.min(x))/(np.max(x)-np.min(x))
-def make_gaussian_iter(mu,sd,stim_max=180):
+def make_gaussian_iter(mu,sd,stim_max=360):
     if np.isscalar(mu):
         mu=[mu]
     if np.isscalar(sd):
         sd=[sd]
     return np.array([normalize(np.roll(signal.gaussian(stim_max, std=s),m-stim_max//2)) for m,s in zip(mu,sd)]).T
-def make_noncirc_gaussian_iter(mu,sd,stim_max=180):
+def make_noncirc_gaussian_iter(mu,sd,stim_max=360):
     if np.isscalar(mu):
         mu=[mu]
     if np.isscalar(sd):
@@ -55,20 +70,26 @@ def circ_diff(a,b,r=360):
         elif diff[k] > r//2:
             diff[k] = b[k] - a[k] - r
     return diff
+def find_vm_sd(channel_responses,plotting=False):
+    # fits to von Mises distribution
+    xReal = np.arange(360)
     
-def rotate(l,dist):
-    # this is identical but faster than np.roll() for larger matrices 
-    l=l.tolist()
-    for i,d in enumerate(dist):
-        ll = l[i]
-        for _ in range(d):
-            ll.append(ll.pop(0))
-    return np.array(l)
+    f = sp.interpolate.interp1d(np.arange(len(channel_responses)),channel_responses)
+    yReal = f(np.arange(0,len(channel_responses)-1,(len(channel_responses)-1)/360))
+    yReal = np.roll(yReal,180-np.argmax(yReal))
+    
+    losses = [np.abs(np.sum(yReal - make_gaussian_iter(180,sd))) for sd in np.arange(10,180)]
+
+    if plotting:
+        plt.plot(yReal)
+        plt.plot(make_gaussian_iter(180,np.arange(10,180)[np.argmin(losses)]))
+        
+    return np.arange(10,180)[np.argmin(losses)]
 
 ## Main functions
 def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
                       nchannels=9,channel_sd=None,channel_mus=None,
-                      plot_basis_set=False,calc_confidence=True):
+                      plot_basis_set=False):
     """
     Input trial by voxel matrix (or trial by electrode/component/etc.) and the array of presented stimuli and returns trial-by-trial predicted stimuli, confidence values (goodness of fits), and (non-aligned) reconstructions.
 
@@ -81,19 +102,17 @@ def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
     stim_max : int, optional
         Specifies the range of stimulus space, such that all features are assumed to be within the range of zero to stim_max-1. E.g., if studying color within a 360° stimulus space, you must change this value to 360 and ensure that all features are integers within 0-359. Defaults to 0-179° stimulus space.
     is_circular: bool, optional
-        Specifies whether the stimulus space is circular or not. Defaults to True. E.g., for orientation and color, stimulus space is typically circular (170° and 0° are the same distance away from 175°, assuming stim_max=180); however, for spatial location, you would typically not want a circular stimulus space. 
+        Specifies whether the stimulus space is circular or not. Defaults to True. E.g., for orientation and color, stimulus space is typically circular (170° and 0° are the same distance away from 180°, assuming stim_max=180); however, for spatial location, you would typically not want a circular stimulus space. 
     nfolds : int, optional
         Number of folds of cross-validation. Defaults to 10.
     nchannels : int, optional
         Number of basis channels in basis set. Note that iterative shifting will be applied such that reconstructions will always be of stim_max length, regardless of number of channels specified. Defaults to 9.
     channel_sd : float, optional
-        The standard deviation of each channel of the basis set. Defaults to standard deviation of a cosine raised to nchannels-1. Note that IEM() runs noticeably quicker if channel_sd is specified!
+        The standard deviation of each channel of the basis set. Defaults to cosine raised to nchannels-1, or approximately 39° SD.
     channel_mus : array_like, optional
         The centers of each channel. Defaults to equally spaced channels. Due to iterative shifting, the centers of each channel are inconsequential if assuming equal spacing. However, if you do not want equally spaced channels (e.g., one channel is always farther apart from the other channels), then provide an array of the centers of each channel (in stimulus space). Note that iterative shifting is applied in all cases.
     plot_basis_set: bool, optional
         Optionally plot the basis set by setting to True. Only plots the first iteration of iterative shifting procedure. Defaults to False.
-    calc_confidence: bool, optional
-        Whether to return goodness of fits or not. If True, IEM() returns (predictions, confidences, recons, aligned_recons). If False, returns (predictions, recons, aligned_recons). Defaults to True.
 
     Returns
     -------
@@ -103,19 +122,18 @@ def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
         The goodness of fit for each trial. That is, the correlation coefficient (Pearson's R) for each trial when performing the "correlation table" procedure to estimate each trial's predicted stimulus. You may choose to exclude some trials with the lowest confidences to potentially increase statistical power.
     recons : matrix (ndarray)
         Returns trial-by-trial reconstructions as a matrix of shape num_trials by stim_max. Note that these reconstructions are not aligned: the maximum point of a single trial reconstruction is generally associated with that trial's predicted color. You can visualize each trial reconstruction by plotting (e.g., plt.plot(recons[0,:]) for the first trial).
-    aligned_recons: matrix (ndarray)
-        Returns trial-by-trial reconstructions aligned such that the ground-truth stimuli are zero. That is, if reconstructions are perfect, each trial reconstruction should be centered at zero along the x-axis when plotted, e.g., plt.plot(aligned_recons[trial,:]).
 
     Example
     ----------
     >>> from inverted_encoding import IEM
     >>> predictions, confidences, recons = IEM(trialbyvoxel,features,stim_max=180,is_circular=True)
     """
+
+    np.random.seed(1) # make procedure reproducible by setting rng
     ntrials = len(features)
     
     # ensure stimulus features are integers less than stim_max
-    if not (issubclass(type(features[0]), np.integer) or type(features[0])==int):
-        raise Exception('All features must be integers within range 0 to stim_max-1. If your stimulus space requires decimal precision, you can multiply your array by some order of magnitude, run this function, and then undo the transformation afterwards.')
+    features = features.astype(int)
     if is_circular:
         features[features==stim_max]=0
     elif np.max(features)==stim_max:
@@ -132,12 +150,13 @@ def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
     if trialbyvoxel.shape[0]!=ntrials:
         raise Exception('number of trials is not consistent between trialbyvoxel and features!')
 
-    # prep basis set
+    # prep basis set, sd = cosine raised to nchannel-1
     if channel_sd==None:
-        sd = 42 * stim_max//360 #(sd = approximately cosine raised to nchannel-1)
+        sd = find_vm_sd(np.cos(np.deg2rad(np.arange(180)))**(nchannels-(nchannels%2)))
+        sd = sd * stim_max/360
     else:
         sd = channel_sd
-    if np.all(channel_mus==None):
+    if channel_mus==None:
         est_mus = np.linspace(0,(stim_max-(stim_max/nchannels)),nchannels).astype(int)
     else:
         est_mus = channel_mus
@@ -157,8 +176,7 @@ def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
 
     # define empty variables
     predictions = np.full(ntrials,np.nan)
-    if calc_confidence:
-        confidences = np.full(ntrials,np.nan)
+    confidences = np.full(ntrials,np.nan)
     recons = np.full((ntrials,stim_max),np.nan)
 
     kf = KFold(n_splits=nfolds)
@@ -181,28 +199,21 @@ def IEM(trialbyvoxel,features,stim_max=180,is_circular=True,nfolds=10,
             est_colors = np.argmax(generate_correlation_map(cr_tsts,
                                         make_gaussian_iter(np.arange(stim_max),
                                                    sd*np.ones(stim_max),stim_max)),axis=1)
-            if calc_confidence:
-                precisions = np.array([sp.stats.linregress(make_gaussian_iter([est_colors[t]],
+            precisions = np.array([sp.stats.linregress(make_gaussian_iter([est_colors[t]],
                                                    sd*np.ones(stim_max),stim_max).flatten(),
                                                    cr_tsts[t,:]).rvalue for t in range(len(tstf))])
         else:
             est_colors = np.argmax(generate_correlation_map(cr_tsts,
                                         make_noncirc_gaussian_iter(np.arange(stim_max),
                                                    sd*np.ones(stim_max),stim_max)),axis=1)
-            if calc_confidence:
-                precisions = np.array([sp.stats.linregress(make_noncirc_gaussian_iter([est_colors[t]],
+            precisions = np.array([sp.stats.linregress(make_noncirc_gaussian_iter([est_colors[t]],
                                                    sd*np.ones(stim_max),stim_max).flatten(),
                                                    cr_tsts[t,:]).rvalue for t in range(len(tstf))])
         predictions[test_index] = est_colors
-        if calc_confidence:
-            confidences[test_index] = precisions
+        confidences[test_index] = precisions
         recons[test_index,:] = cr_tsts
-    # align the reconstructions at zero, such that ideally reconstruction is centered at 0 when plotted
-    aligned_recons = rotate(recons,features)
-    if calc_confidence:
-        return predictions, confidences, recons, aligned_recons
-    else:
-        return predictions, recons, aligned_recons
+        
+    return predictions, confidences, recons
 
 def permutation(features,stim_max=180,num_perm=5000,is_circular=True):
     """
@@ -229,6 +240,7 @@ def permutation(features,stim_max=180,num_perm=5000,is_circular=True):
     >>> from inverted_encoding import permutation
     >>> null_mae_distribution = permutation(features,stim_max=180,is_circular=True)
     """
+    np.random.seed(1) # make procedure reproducible by setting rng
     ntrials = len(features)
     
     # ensure stimulus features are integers less than stim_max
