428372829863995004fa51130fcb7e196a50081d
Paul Scotti <paulpauzle@gmail.com>
2022-07-16 11:16:48 -0400

    version 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name='inverted_encoding',
-    version='0.2.2',
+    version='0.2.3',
     description='Implementation of inverted encoding model as described in Scotti, Chen, & Golomb',
     packages=['inverted_encoding'],
     url="https://github.com/paulscotti/inverted_encoding",
